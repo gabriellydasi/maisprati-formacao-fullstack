1fae8e5ae34023db17675f9d2cde05cdb95a8680
Gabrielly <gabriellysilva.oli01@gmail.com>
2024-07-28 12:50:03 -0300

    print the first 50 prime numbers greater than 100

--- modules/module 01/class 7/activities/activity4.js
+++ modules/module 01/class 7/activities/activity4.js
@@ -1,2 +1,31 @@
 // Escreva um algoritmo para imprimir os 50 primeiros número primos maior que 100.
 // Obs.: Número primo é aquele divisível somente por 1 e ele mesmo.
+
+let count = 0;
+let num = 101; // maior que 100 
+
+while(count < 50){
+    let isPrime = true; 
+
+    if(num <= 1){
+        isPrime = false;
+    }else if(num === 2){
+        isPrime = true;
+    }else if(num % 2 === 0){
+        isPrime = false;
+    }else{
+        for(let i = 3; i <= Math.sqrt(num); i += 2){
+            if(num % i === 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if(isPrime){
+        console.log(num);
+        count++;
+    }
+
+    num++;
+}

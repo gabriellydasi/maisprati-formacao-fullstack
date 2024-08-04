11820f4ababb582ac7b267d4533cbd07f971e3b1
Gabrielly <gabriellysilva.oli01@gmail.com>
2024-08-04 19:17:33 -0300

    Removing files

--- modules/module 01/class 7/activities/activity5.js
+++
@@ -1,8 +0,0 @@
-// Solicitar números ao usuário até que ele insira um valor negativo 
-
-const prompt = require('prompt-sync')()
-
-let num 
-do{
-    num = Number(prompt('Digite um número: '))
-}while(num >= 0)
--- modules/module 01/class 9/activities/activity1.js
+++
@@ -1,9 +0,0 @@
-// 1) Soma dos elementos de um array
-let sum = 0
-let arr = [10, 20, 30, 40]
-
-for(let i = 0; i < arr.length; i++){
-    sum += arr[i]
-}
-
-console.log(`Soma = ${sum}`)

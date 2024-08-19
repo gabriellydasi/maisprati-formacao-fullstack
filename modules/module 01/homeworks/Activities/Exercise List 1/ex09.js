03004e6e45d49a233f4090ca60a9c46417220c1e
Gabrielly <gabriellysilva.oli01@gmail.com>
2024-08-19 13:04:54 -0300

    Countdown

---
+++ modules/module 01/homeworks/Activities/Exercise List 1/ex09.js
@@ -0,0 +1,10 @@
+/*
+    9. Implemente um programa que exibe uma contagem regressiva de 10 até 1 no console utilizando um loop for.
+*/
+
+let number = 10;
+
+for(let count = 0; count < 10; count++){
+    console.log(number);
+    number--;
+}
